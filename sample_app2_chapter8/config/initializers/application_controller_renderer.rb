694138e9b2f374e45c9965e52482cfcc9e8d0f3e
afew3232 <afew3232@gmail.com>
2020-02-04 13:30:34 +0900

    [Finish] 3-8

---
+++ sample_app2_chapter8/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f960b2ed616dd7a5c2a1c9fe9f00cab3c5f59eeee6651ff23b4ac572abb6d68621ff36cf538cd13f0bbd451bc0cf6f5e33caf1711b237fdfb01af3a2a694afce'
+
